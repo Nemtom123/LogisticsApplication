05654e04749c0dd46f6bffd32a44f1a4eb2c462c
Nemtom123 <dobiasz.tamas@gmail.com>
2021-06-13 08:08:16 +0200

    findByMilestoneId and transportPlanId correction of

--- src/main/java/hu/webuni/logistics/dobiasz/repository/SectionRepository.java
+++ src/main/java/hu/webuni/logistics/dobiasz/repository/SectionRepository.java
@@ -11,13 +11,13 @@ import hu.webuni.logistics.dobiasz.model.Section;
 public interface SectionRepository extends JpaRepository<Section, Long>{
 
 	//@Query("SELECT s FROM Section s WHERE s.transport_plan_id = transport_plan_id AND s.from_milestone_id = 1 OR s.to_milestone_id = 1")
-	@Query("SELECT s FROM Section s WHERE s.transport_plan_id = :transportPlanId AND s.from_milestone.id = :milestoneId OR s.to_milestone.id = :milestoneId")
+	@Query("SELECT s FROM Section s WHERE s.transportPlan.id = :transportPlanId AND s.fromMilestone.id = :milestoneId OR s.toMilestone.id = :milestoneId")
 	List<Section> findByTransportPlanAndMilestone(long transportPlanId, long milestoneId);
 
-	@Query("SELECT s FROM Section s WHERE s.transport_plan_id = :transportPlanId AND s.number = :number")
+	@Query("SELECT s FROM Section s WHERE s.transportPlan.id = :transportPlanId AND s.number = :number")
 	Optional<Section> findByTransportPlanIdAndNumber(long transportPlanId, int number);
 
-	@Query("SELECT s FROM Section s WHERE s.from_milestone.id = :milestoneId OR s.to_milestone.id = :milestoneId")
+	@Query("SELECT s FROM Section s WHERE s.fromMilestone.id = :milestoneId OR s.toMilestone.id = :milestoneId")
 	Optional<Section> findByMilestoneId(long milestoneId);
 
 }
